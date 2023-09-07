9ca0fc249a29df4b1ca60a046ffe2bb3dd449939
devshinn <shin.github@gmail.com>
2023-09-07 09:30:24 +0900

    Fix: api 주소 분리 .env

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,9 @@
 import { Sick } from '../types';
 
+const API_URL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_PROD_API_URL
+    : process.env.REACT_APP_DEV_API_URL;
 export const getData = async (query: string): Promise<Sick[]> => {
-  return await fetch(`http://localhost:4000/sick?q=${query}`).then(response => response.json());
+  return await fetch(`${API_URL}/sick?q=${query}`).then(response => response.json());
 };
